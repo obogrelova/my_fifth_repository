3b974d3417c2dfd1033015d561a0e877b486c21b
obogrelova <on.obogrelova@gmail.com>
2024-10-16 20:10:37 +0500

    change in code new_2

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-first_day_of_summer = today.month == 6 and today.day == 1
-if first_day_of_summer:
-    print("First day of summer")
+first_day_of_autumn = today.month == 9 and today.day == 1
+if first_day_of_autumn:
+    print("First day of autumn")
